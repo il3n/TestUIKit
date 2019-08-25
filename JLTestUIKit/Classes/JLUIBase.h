0030d23402b3c2a2915112ac729c8ae17c466d84
lee <>
2019-08-25 21:27:58 +0800

    ss

---
+++ JLTestUIKit/Classes/JLUIBase.h
@@ -0,0 +1,16 @@
+//
+//  JLUIBase.h
+//  JLTestUIKit
+//
+//  Created by lee on 2019/8/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JLUIBase : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JLTestUIKit/Classes/JLUIBase.m
@@ -0,0 +1,12 @@
+//
+//  JLUIBase.m
+//  JLTestUIKit
+//
+//  Created by lee on 2019/8/25.
+//
+
+#import "JLUIBase.h"
+
+@implementation JLUIBase
+
+@end
